5faf95bbb5814a1ea873940d5c512d25e4a04399
Zabyaaboaljadayel <38957489+Zabyaaboaljadayel@users.noreply.github.com>
2022-10-09 12:42:11 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import styles from "../styles/Home.module.css";
 import LoginUser from "../src/components/login";
 import LoginCom from "../src/components/loginCompany";
 import Overlay from "../src/components/overlay";
+import SignUpUser from "../src/components/SignUpUser";
+import signUpCompanies from "../src/components/SignUpCompanies";
 
 export default function Home() {
   return (
@@ -14,3 +16,13 @@ export default function Home() {
     </div>
   );
 }
+export default function Home() {
+  return (
+    <div className={styles.container}>
+          <SignUpUser />
+          <SignUpCompanies />
+      <Overlay />
+    </div>
+  );
+}
+
