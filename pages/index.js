b39dbc440b1ea9b0b38058c67c3bfe126e3e8787
sonia <sonialalani7860@gmail.com>
2022-10-10 09:33:17 +0100

    son

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import LoginForm from "../src/components/login";
+import LoginUser from "../src/components/login";
 import Overlay from "../src/components/overlay";
 
 export default function Home() {
---
+++ src/components/input.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+function input() {
+  const [inputs, setInputs] = React.useState({
+    User: "",
+    Password: "",
+  });
+}
